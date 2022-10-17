4cce545997d4f06a43f509b4ddaa9d381a1929fc
OrionShock <orionshock@gmail.com>
2022-10-16 21:35:50 -0700

    useLDB

--- BetterRaidInfoFrame.lua
+++ BetterRaidInfoFrame.lua
@@ -1,6 +1,6 @@
 local ADDON_NAME, core = ...
 local LibQTip = LibStub("LibQTip-1.0")
-
+local betterRaidInfoQTip
 local Debug = function()
 end
 
@@ -17,37 +17,111 @@ eventFrame:SetScript(
         end
     end
 )
-
-
+eventFrame:RegisterEvent("ADDON_LOADED")
+eventFrame:RegisterEvent("UPDATE_INSTANCE_INFO")
 
 function core:ADDON_LOADED(event, addon)
     if addon == ADDON_NAME then
         core.oldFunc_RaidFrameRaidInfoButton_OnClick = RaidFrameRaidInfoButton:GetScript("OnClick")
         RaidFrameRaidInfoButton:SetScript("OnClick", core.toggleBetterRaidInfoFrame)
     end
+    RequestRaidInfo()
 end
 
 local savedInstanceInfo = {}
 local savedInstance_TopLevelGroups = {}
 
-local function UpdateSavedInstanceInfo()
-    local function instanceSortByNameFunction(a, b)
-        return a.name > b.name
-    end
+local function instanceSortByNameFunction(a, b)
+    return a.name > b.name
+end
+
+function core:UPDATE_INSTANCE_INFO()
     for i = 1, GetNumSavedInstances() do
         local name, id, reset, difficulty, locked, extended, instanceIDMostSig, isRaid, maxPlayers, difficultyName, numEncounters, encounterProgress, extendDisabled = GetSavedInstanceInfo(i)
         if name then
             savedInstanceInfo[difficultyName] = savedInstanceInfo[difficultyName] or {}
-            table.insert(savedInstanceInfo[difficultyName], {name = name, id = id, reset = reset, numEncounters = numEncounters, encounterProgress = encounterProgress})
+            table.insert(
+                savedInstanceInfo[difficultyName],
+                {
+                    ["name"] = name,
+                    ["id"] = id,
+                    ["reset"] = reset,
+                    ["numEncounters"] = numEncounters,
+                    ["encounterProgress"] = encounterProgress,
+                    ["difficultyName"] = difficultyName
+                }
+            )
         end
     end
     for difficultyName, infoTable in pairs(savedInstanceInfo) do
         table.insert(savedInstance_TopLevelGroups, difficultyName)
         table.sort(infoTable, instanceSortByNameFunction)
     end
+    table.sort(savedInstance_TopLevelGroups)
 end
 
+local function closeWindow()
+    betterRaidInfoQTip:Release()
+    betterRaidInfoQTip = nil
+end
 
-function core.toggleBetterRaidInfoFrame(frame,button,...)
-    print(frame,button,...)
+local function populateTooltip(window) --yes we call it a window here... sue me
+    --we are 5 columns Wide
+    window:AddHeader(RAID_INFORMATION)
+    window:SetCell(1, 1, RAID_INFORMATION, nil, "LEFT", 4)
+    window:SetCell(1, 5, "[X]", nil, "RIGHT")
+    window:SetCell(1, 6, " ")
+    window:SetCellScript(1, 5, "OnMouseUp", closeWindow)
+
+    window:AddLine(RAID_INFO_DESC)
+    window:SetCell(2, 1, RAID_INFO_DESC, nil, "LEFT", 5)
+
+    window:AddLine()
+    window:AddHeader(INSTANCE, "Difficulty", "Remaining Time", "Encounters", "RaidID")
+    window:AddLine()
+
+    for index, difficultyName in ipairs(savedInstance_TopLevelGroups) do
+        local currentDifficultyInfo = savedInstanceInfo[difficultyName]
+        if next(currentDifficultyInfo) then
+            local cLine = window:AddHeader(difficultyName)
+            window:SetCell(cLine, 1, difficultyName, nil, "LEFT", 5)
+        end
+        for dIndex, eventInfo in ipairs(currentDifficultyInfo) do
+            window:AddLine(
+                currentDifficultyInfo.name,
+                currentDifficultyInfo.difficultyName,
+                SecondsToTime(currentDifficultyInfo.reset),
+                string.join("/", currentDifficultyInfo.encounterProgress, currentDifficultyInfo.numEncounters),
+                currentDifficultyInfo.id
+            )
+        end
+    end
+end
+
+local function MakeAndPopulateQTip()
+    if betterRaidInfoQTip then
+        print("WTF??")
+        return
+    end
+    betterRaidInfoQTip = LibQTip:Acquire("betterRaidInfo", 6, "LEFT")
+    if not betterRaidInfoQTip then
+        print("??WTF??")
+        return
+    end
+    RequestRaidInfo()
+    populateTooltip(betterRaidInfoQTip)
+    betterRaidInfoQTip:SetPoint("TOPLEFT", FriendsFrame, "TOPRIGHT")
+    betterRaidInfoQTip:Show()
+end
+
+function core.toggleBetterRaidInfoFrame(frame, button, ...)
+    if IsShiftKeyDown() then
+        core.oldFunc_RaidFrameRaidInfoButton_OnClick(frame, button, ...)
+        return
+    end
+    if not betterRaidInfoQTip then
+        MakeAndPopulateQTip()
+    else
+        closeWindow()
+    end
 end
